3f9b4815afb0700db41340e91af2b4b4d35451eb
Cirillio <ineed2healing@gmail.com>
2025-02-06 01:54:29 +0700

    init

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+This file explains how Visual Studio created the project.
+
+The following tools were used to generate this project:
+- create-vite
+
+The following steps were used to generate this project:
+- Create vue project with create-vite: `npm init --yes vue@latest nft_marketplace -- --eslint `.
+- Updating vite.config.js with port.
+- Create project file (`nft_marketplace.esproj`).
+- Create `launch.json` to enable debugging.
+- Add project to solution.
+- Write this file.
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+<template>
+  <div
+    class="flex w-full h-24 items-center justify-between px-12 py-5 text-text work-sans font-semibold"
+  >
+    <div class="flex gap-4 items-center">
+      <img src="/public/icons/Storefront.svg" alt="logo" />
+      <span class="space-mono text-xl">NFT Marketplace</span>
+    </div>
+    <div class="flex gap-2.5 items-center">
+      <ul class="flex gap-2.5">
+        <li class="py-3 px-5">Marketplace</li>
+        <li class="py-3 px-5">Rankings</li>
+        <li class="py-3 px-5">Connect a wallet</li>
+      </ul>
+      <button class="flex gap-2 items-center justify-center bg-action py-4 px-6 rounded-2xl">
+        <img src="/public/icons/User.svg" alt="user" />
+        <p>Sign Up</p>
+      </button>
+    </div>
+  </div>
+</template>
